02dcfa0b13537eee656d6acd3dfe13272d7d7cdb
rahul-kumar25 <rahul.kumar@clivertech.com>
2026-02-14 17:36:21 +0530

    update

--- src/main/java/com/hibernet/main/Main.java
+++ src/main/java/com/hibernet/main/Main.java
@@ -3,10 +3,7 @@ package com.hibernet.main;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 import com.hibernet.entity.Employee;
 
@@ -18,13 +15,20 @@ public class Main {
 
  Employee e = new Employee("Rahul thakur kumar", "male", 9999999);
  
+ 
+// method1
+ Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+ SessionFactory sf = cfg.buildSessionFactory();
+ Session session = sf.openSession();
+ Transaction tx = session.beginTransaction();
 
-	StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
-	Metadata metadata = new MetadataSources(ssr).getMetadataBuilder().build();
-	SessionFactory sf = metadata.buildSessionFactory();
-	Session session = sf.openSession();
-	Transaction tx = session.beginTransaction();
-	
+ //method2
+//	StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+//	Metadata metadata = new MetadataSources(ssr).getMetadataBuilder().build();
+//	SessionFactory sf = metadata.buildSessionFactory();
+//	Session session = sf.openSession();
+//	Transaction tx = session.beginTransaction();
+//	
 	
 	session.persist(e);
 	tx.commit();
