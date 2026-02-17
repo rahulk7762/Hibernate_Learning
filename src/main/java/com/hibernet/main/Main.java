f8d0e5faf8b6cda533a9dafbf22e5a7cc06da275
rahul-kumar25 <rahul.kumar@clivertech.com>
2026-02-17 17:18:25 +0530

    update

--- src/main/java/com/hibernet/main/Main.java
+++ src/main/java/com/hibernet/main/Main.java
@@ -21,95 +21,11 @@ public class Main {
 	Transaction tx = session.beginTransaction();
 	
 	
-//	-------------------------------------------------1.------------------------------------------
-//	Query query = session.createQuery("from myemp");  //or
-//	Query query = session.createQuery("from myemp",Employee.class);
-//	
-//	List list = query.list();
-//	System.out.println(list);
+
 	
-//---------------------------------2.---------------------------------------------------------------
-//	Query query = session.createQuery("from myemp",Employee.class);
-//	query.setFirstResult(1);
-//	query.setMaxResults(2);
-//	List list = query.list();
-//	System.out.println(list);
-	
-	
-	//----------------------------------------3--------------------------
-//	Query query = session.createQuery("update myemp set name=:n where id=:i");
-//	query.setParameter("n", "Aman Kumar");
-//	query.setParameter("i",4);
-//	int status = query.executeUpdate();
-//	System.out.println(status);
-//	
-	
-	//-------------------------4---------------------
-//	Query query = session.createQuery("delete from myemp where id=:i");
-//	query.setParameter("i", 1);
-//	int result = query.executeUpdate();
-//	System.out.println("Row Deleted"+result);
-//	tx.commit();
-//	
-	
-//	-------------------------5----------------------------
-//	Query query = session.createQuery("select sum(salary) from myemp");
-//	List<Integer> list= query.list();
-//	System.out.println(list.get(0));
-	
-	
-// --------------------------- 6. To Assingn the Alias-------------------------------
-//	String alias = "from myemp as e";
-//	Query query = session.createQuery(alias);
-//	List list = query.list();
-//	System.out.println(list);
-	
-	
-	
-	
-	// select
-//	String hql = "select e.name from myemp e";
-//	Query query = session.createQuery(hql);
-//	List list = query.list();
-//	System.out.println(list);
-	
-	
-//	Select multiple items and show them using the list
-//	String hql = "select e.name, e.gender from myemp e";
-//	Query query = session.createQuery(hql);
-//	List list = query.list();
-//	System.out.println(list);
-//	System.out.println(list.get(0));
-	
-	
-//	
-//	
-//	String hql = "from myemp e where e.id = :id"; // or select e from MyEmp e where e.id = :id  :- Both are same
-//
-//	Query query = session.createQuery(hql);
-//	query.setParameter("id", 1);
-//
-//	List list = query.list();
-//	System.out.println(list);
-	
-	
-//	Named Parameters
-//	String hql = "from myemp e where e.id=:id";
-//	Query query = session.createQuery(hql);
-//	query.setParameter("id",1);
-//	List list = query.list();
-//	System.out.println(list);
-	
-	
-//	Aggregate functions
-//	String hql = "select sum(salary) from myemp";
-//	String hql = "select count(distinct e.name) from myemp e";
-//	String hql = "select count(distinct e.salary) from myemp e";
-//	String hql = "select sum(distinct e.salary) from myemp e";
-//	Query query = session.createQuery(hql);
-//	List list = query.list();
-//	System.out.println(list);
-	
+
+
+
 	
 	
 	
